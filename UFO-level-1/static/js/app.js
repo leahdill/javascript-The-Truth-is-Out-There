e36389d894592b3baba47423962ef921947ab298
Leah Dillard <77127752+leahgraced@users.noreply.github.com>
2021-06-19 16:34:00 -0600

    Filters Work

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,34 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDateField = d3.select("#datetime");
+var inputCityField = d3.select("#city");
+var cols = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        cols.forEach(col => row.append("td").text(ufoSightings[col])
+        )
+    });
+}
+
+addData(tableData);
+
+button.on("click", () => {
+    d3.event.preventDefault();
+    var inputDate = inputDateField.property("value").trim();
+    var filterDates = tableData.filter(tableData => tableData.datetime === inputDate);
+    $tbody.html("");
+    let response = {
+        filterDates
+    }
+    if(response.filterDates.length !== 0) {
+        addData(filterDates);
+    }
+        else {
+            $tbody.append("tr").append("td").text("No Supernatural Events Here...Yet");
+        }
+})
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,43 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDateField = d3.select("#datetime");
+var inputCityField = d3.select("#city");
+var cols = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        cols.forEach(col => row.append("td").text(ufoSightings[col])
+        )
+    });
+}
+
+addData(tableData);
+
+button.on("click", () => {
+    d3.event.preventDefault();
+    
+    var inputDate = inputDateField.property("value").trim();
+    var inputCity = inputCityField.property("value").toLowerCase().trim();
+    var filterDates = tableData.filter(tableData => tableData.datetime === inputDate);
+    var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+    var filterComboData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
+
+    $tbody.html("");
+
+    let response = {
+        filterDates, filterCity, filterComboData
+    }
+    if(response.filterComboData.length !== 0) {
+        addData(filterComboData);
+    }
+        else if(response.filterComboData.length === 0 && ((response.filterDates.length !== 0 || response.filterCity.length !== 0))) {
+            addData(filterDates) || addData(filterCity);
+        }
+        else {
+            $tbody.append("tr").append("td").text("No Supernatural Events Here...Yet");
+        }
+})
